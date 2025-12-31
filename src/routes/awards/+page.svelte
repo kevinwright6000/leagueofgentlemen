c5186ed911bdc1e764f585fd7b243378daacbc99
kevinwright6000 <43287275+kevinwright6000@users.noreply.github.com>
2025-12-30 21:20:19 -0700

    Update +page.svelte

--- src/routes/awards/+page.svelte
+++ src/routes/awards/+page.svelte
@@ -5,6 +5,11 @@
 
     export let data;
     const {awardsData, teamManagersData} = data;
+
+const goToSection = (section) => {
+        const top = section.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({left: 0, top, behavior: 'smooth'});
+    }
 </script>
 
 <style>
@@ -32,6 +37,85 @@
 		margin: 80px auto;
 		text-align: center;
 	}
+
+	.awards {
+        position: relative;
+        z-index: 1;
+        width: 92%;
+        max-width: 800px;
+        margin: 8em auto 10em;
+    }
+
+    h1 {
+        font-size: 2em;
+        line-height: 1.2em;
+        text-align: center;
+        margin: 2em 0 1.5em;
+    }
+
+    h2 {
+        font-size: 1.5em;
+        line-height: 1.2em;
+    }
+
+    h3 {
+        text-decoration: underline;
+        font-size: 1.3em;
+        line-height: 1.2em;
+    }
+
+    h4 {
+        text-decoration: underline;
+        margin-left: 2em;
+        font-size: 1.2em;
+        line-height: 1.2em;
+    }
+
+    h5 {
+        margin-left: 6em;
+        font-size: 0.8em;
+        line-height: 1.1em;
+    }
+
+    .subBlock {
+        margin-left: 2.4em;
+    }
+
+    .sectionHeading {
+        margin: 4em 0 1.5em;
+    }
+
+    .subSectionHeading {
+        margin: 1.5em 0 1.5em;
+    }
+
+    .underscore {
+        text-decoration: underline;
+    }
+
+    .right {
+        text-align: right;
+    }
+
+    .positionMaximums td {
+        min-width: 3em;
+    }
+
+    .noUnderscore {
+        text-decoration: none;
+    }
+
+    .clickable {
+        cursor: pointer;
+    }
+
+    .clickable:hover {
+        color: #00316b;
+    }
+
+    p {
+        color: #777;
+    }
 </style>
 
 <div class="awards">
@@ -51,3 +135,100 @@
 		<p>Something went wrong: {error.message}</p>
 	{/await}
 </div>
+
+
+
+<div class="previous years awards">
+    <h1 class="noUnderscore">Previous Year AwardsN</h1>
+    
+    <h2 class="noUnderscore">Years</h2>
+    
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(one)}>2025:</h3>
+     
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(two)}>2024</h3>
+  
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(three)}>2023</h3>
+   
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(four)}>2022</h3>
+  
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(five)}>2021</h3>
+    
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(six)}>2020</h3>
+   
+    <h3 class="noUnderscore clickable" onclick={() => goToSection(seven)}>2019</h3>
+
+	<h3 class="noUnderscore clickable" onclick={() => goToSection(eight)}>2018</h3>
+    
+    <hr />
+
+<h3 bind:this={one}>2025</h3>
+    <p>Season 2025:</p>
+
+    <ol>
+        <li>Champion: Kevin</li>
+        <li>Runner-Up: Steven</li>
+        <li>Spud: Mario</li>
+    </ol>
+
+<h3 bind:this={two}>2024</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Zach</li>
+        <li>Runner-Up: ReRe</li>
+        <li>Spud: Bgar</li>
+    </ol>
+
+<h3 bind:this={three}>2023</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Zach</li>
+        <li>Runner-Up: ReRe</li>
+        <li>Spud: Tim</li>
+    </ol>
+
+<h3 bind:this={four}>2022</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Steven/Tyler - Tie</li>
+        <li>Runner-Up: Tie</li>
+        <li>Spud: Zach</li>
+    </ol>
+
+<h3 bind:this={five}>2021</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Ryan</li>
+        <li>Runner-Up: Bgar</li>
+        <li>Spud: Mario</li>
+    </ol>
+
+<h3 bind:this={six}>2020</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Troy</li>
+        <li>Runner-Up: Kevin</li>
+        <li>Spud: Blake</li>
+    </ol>
+
+<h3 bind:this={seven}>2019</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Justin</li>
+        <li>Runner-Up: Troy</li>
+        <li>Spud: Kevin</li>
+    </ol>
+
+<h3 bind:this={eight}>2018</h3>
+    <p>Season 2024:</p>
+
+    <ol>
+        <li>Champion: Kevin</li>
+        <li>Runner-Up: Tyler</li>
+        <li>Spud: Matt</li>
+    </ol>
