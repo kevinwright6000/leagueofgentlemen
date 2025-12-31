f980ed95a708ee99697647b25f18267f4b2cfb54
kevinwright6000 <43287275+kevinwright6000@users.noreply.github.com>
2025-12-30 21:06:27 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -10,7 +10,7 @@ export const homepageText = `
   <p>The League of Honorable Degenerates was formed from the ashes of a defunct league back in 2012.</p>
   <p>After 13 years of existing. The league is being reinvented.</p>
   <p>No longer will we live in a facebook chat and the NFL site.</p>
-  <p>The League of Gentlemen will have a new home here, connected to the sleeper app for fantasy team management.</p>
+  <p>The League of Honorable Degenerates will have a new home here, connected to the sleeper app for fantasy team management.</p>
 `;
 
 /*   STEP 3   */
