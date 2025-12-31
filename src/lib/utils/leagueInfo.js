fe8808091c653a864f185fc78af991074acdc17c
kevinwright6000 <43287275+kevinwright6000@users.noreply.github.com>
2025-12-30 20:50:54 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1311924460936249344"; // your league ID
-export const leagueName = "League of Gentlemen"; // your league name
+export const leagueName = "The League of Honorable Degenerates"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
